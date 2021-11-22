b4c29278b5764411545bb1f5af5f9588fe19ff58
RkSabando <ralph.k.sabando@accenture.com>
2021-11-22 13:12:25 +0800

    added logout feature

--- src/app/user/layout/header/header.component.ts
+++ src/app/user/layout/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { USER_ROUTES } from 'src/app/shared/enums/routes.enum';
+import { Router } from '@angular/router';
+import { DIALOG_TYPES } from 'src/app/shared/enums/dialog.enum';
+import { AUTH_ROUTES, USER_ROUTES } from 'src/app/shared/enums/routes.enum';
+import { DialogOptions } from 'src/app/shared/model/dialog-model';
+import { CustomDialogService } from 'src/app/shared/services/custom-dialog.service';
 import { StorageService } from 'src/app/shared/services/storage.service';
 
 @Component({
@@ -12,10 +16,28 @@ export class HeaderComponent implements OnInit {
   public user = this.storage.getCookie('user');
   public ROUTES = USER_ROUTES;
   constructor(
-    private storage: StorageService
+    private storage: StorageService,
+    private dialog: CustomDialogService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
+  }
+
+  logOut() {
+    const dialogOptions: DialogOptions = {
+      type: DIALOG_TYPES.WARNING,
+      title: 'Warning',
+      desc: `Are you sure you want to log out?`,
+      buttons: ['Yes', 'No']
+    }
+
+    this.dialog.openDialog(dialogOptions,
+      () => {
+        this.storage.deleteCookie('user');
+        this.router.navigate([AUTH_ROUTES.LOGIN]);
+      }
+    );
   }
 
 }
